187c03d96ef576335abd04a1f6c91f5ee8a46413
Christian Berkman <76948509+christianatnacmu@users.noreply.github.com>
2023-06-27 12:48:34 +0300

    Update donorbox-tools.js
    Meter URL now requires donation_meter_color to be set
    HTML changed, regex updated
    Updated function to parse the amount to not need currency symbol

--- donorbox-tools.js
+++ donorbox-tools.js
@@ -6,8 +6,9 @@
 import {fetch} from 'wix-fetch'
 
 const embedUrl = 'https://donorbox.org/embed/';
+const formUrl = 'https://donorbox.org/';
 const hideMeter = 'hide_donation_meter=true';
-const meterOnly = 'only_donation_meter=true';
+const meterOnly = 'donation_meter_color=%23000&only_donation_meter=true';
 
 /**
  * Return URL to embed a donorbox form
@@ -16,7 +17,7 @@ const meterOnly = 'only_donation_meter=true';
  * @param bool showMeter Show the donation meter
  * @return string
  */
-export function embedFormUrl(campaignName, fields, showMeter = false){  
+export function donorboxEmbedUrl(campaignName, fields, showMeter = false){  
     var params = new URLSearchParams(fields).toString()
     if(showMeter == false) params = params + '&' + hideMeter
 
@@ -24,69 +25,100 @@ export function embedFormUrl(campaignName, fields, showMeter = false){
 }
 
 /**
+ * Return URL to a donorbox from
+ * @param string campaignName Donorbox Campaign Name
+ * @param obj fields Field values
+ * @param bool showMeter Show the donation meter
+ * @return string
+ */
+export function donorboxFormUrl(campaignName, fields, showMeter = false){  
+    var params = new URLSearchParams(fields).toString()
+    if(showMeter == false) params = params + '&' + hideMeter
+
+    return formUrl + campaignName + '?' + params
+}
+
+/**
  * Return URL to embed a donorbox donation meter
  * @param string campaignName Donorbox Campaign Name
  * @obj fields FIeld values
  */
-export function embedMeterUrl(campaignName, fields){
+export function donorboxEmbedMeterUrl(campaignName, fields){
     const params = new URLSearchParams(fields).toString()
     return embedUrl + campaignName + '?' + meterOnly + '&' + params
 }
 
-export async function getMeterValues(campaignName){
+/**
+ * Return meter values (goal, amount raised, donations, currency code) as JSON object
+ * @param string campaignName Campaign Name
+ */
+export async function donorboxGetMeterValues(campaignName){
     // Return object
     var returnObj = {
         currencySymbol: null,
         raised: null,
         donations: null,
-        goal: null
+        goal: null,
+        progress: null
     }
 
     // Get Meter HTML content
-    const meterUrl = embedMeterUrl(campaignName)
+    const meterUrl = donorboxEmbedMeterUrl(campaignName, { 'language': 'en', 'time': Date.now() })
     const response = await fetch(meterUrl)
     const html = await response.text()
 
-    // Goal and Currencty Code
-    const goalPattern = /([€$])\s?([0-9.,]+)<\/p><p>Goal/g;
-    const goalMatches = goalPattern.exec(html)
-    if(goalMatches != null){
-        returnObj.currencySymbol = goalMatches[1]
-        returnObj.goal = parseAmountToFloat(goalMatches[2], returnObj.currencySymbol)
+    // Goal, Currencty Symbol, Raised
+    const triplePattern = /raised'><b>([€$])\s?([0-9.,]+)<\/b>\s?\/\s?[€$]\s?([0-9.,]+)/g
+    const tripleMatches = triplePattern.exec(html)
+    if(tripleMatches != null){
+        returnObj.currencySymbol = tripleMatches[1]
+        returnObj.raised = parseAmountToFloat(tripleMatches[2])
+        returnObj.goal = parseAmountToFloat(tripleMatches[3])
     }
-    
-    // Amount Raised
-    const raisedPattern = /([0-9.,]+)<\/p><p>Raised/g;
-    const raisedMatches = raisedPattern.exec(html)
-    if(raisedMatches != null) returnObj.raised = parseAmountToFloat(raisedMatches[1], returnObj.currencySymbol)
 
     // Donations
-    const donationsPattern = /([0-9])<\/p><p>Donations/g;
+    const donationsPattern = /([0-9]+)\s?Donations?/g;
     const donanationsMatches = donationsPattern.exec(html)
     if(donanationsMatches != null) returnObj.donations = parseInt(donanationsMatches[1])
+
+    // Progress
+    if(returnObj.goal != null && returnObj.raised != null) returnObj.progress = parseInt( (returnObj.raised / returnObj.goal) * 100 )
 
     return returnObj
 }
 
 /**
- * Parse amount in EUR or USD to float
- * @param string amountString  Amount as String
- * @param string currencySymbol Currency Symbol
+ * Parse amount to float without knowing type of thousands seperator or decimal point
+ * @param any amount Amount to parse (like 10.500,99 or 12,500/=)
  * @return float
  */
-function parseAmountToFloat(amountString, currencySymbol){
-    switch(currencySymbol){
-        default: // format unkown
-            return parseFloat(amountString)
-        break;
+function parseAmountToFloat(amount){
+	// Return amount if already a number float
+	if(typeof(amount) == 'number') return amount;
 
-        case '€': // format: 12.345,67
-            return parseFloat( amountString.replace('.', '').replace(',', '.') )
-        break;
+	// Remove all characters except numbers, '.' and ','
+	let amountStr = amount.toString()
+	const cleanStr = amountStr.replace(/[^0-9.,]/g, '')
 
-        case '$': // format: 12,345.67
-            return parseFloat(amountString.replace(',', ''));
-        break;
-    }
+	// Search for decimal point in cleanStr
+	const decimalPoint = cleanStr.charAt( cleanStr.length - 3)
+
+	// Extract only integers from cleanStr
+	const amountInt = parseInt( cleanStr.replace(/[^0-9]/g, '') )
+
+	// Create float from amountInt
+	let amountFloat
+
+		// Decimal Point found
+		if(decimalPoint == ',' || decimalPoint == '.'){
+			// Divide by 100 cents to make float
+			amountFloat = parseFloat( amountInt / 100)
+		}
+		// Decimal point not found
+		else{
+			// Create float from amountInt
+			amountFloat = parseFloat(amountInt)
+		}
+
+	return amountFloat
 }
-
